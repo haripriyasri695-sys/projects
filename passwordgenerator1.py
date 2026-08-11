64a4ff1dbfae0aa0ec1158f3b5477913bcdf8e3c
Sri Hari Priya <haripriyasri695@gmail.com>
2026-08-11 23:38:59 +0530

    passwordgeneratorsetup

---
+++ passwordgenerator1.py
@@ -0,0 +1,7 @@
+import random
+letters = "abcdefghijklmnopqrstuvwxyz"
+numbers = "123456789"
+length = int(input("Enter the length of the password: "))
+characters = letters + numbers
+print("characters are ready")
+print("Password length:", length)
